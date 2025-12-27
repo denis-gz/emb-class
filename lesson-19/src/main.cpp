ce902a935122738a822137a533b94323d6942b2c
Denis Z <denis.gz@gmail.com>
2025-12-27 20:26:31 +0200

    [Lesson 19] Refactored to use real time counters (yields more stable PWM frequency)

--- lesson-19/src/main.cpp
+++ lesson-19/src/main.cpp
@@ -3,18 +3,19 @@
 constexpr const int   ADC_PIN   = 6;          // ADC1_5
 constexpr const int   ADC_BITS  = 12;
 constexpr const float ADC_RANGE = 1 << ADC_BITS;
-constexpr const float A_REF     = 1.1f;       // 1.1V
-constexpr const float A_OFFSET  = 0.1f;       // Add 0.1V to the readings
+constexpr const float A_REF     = 0.990f;     // 0.990V (actual measured maximum)
 constexpr const float K         = 4.0f;       // Filter param
 
 constexpr const int CONTROL_PIN = 10;
 
-constexpr const uint32_t TIME_UNIT = 1000;
-constexpr const uint32_t MEASURE_PERIOD = 10  * TIME_UNIT;
-constexpr const uint32_t LOG_PERIOD = 500 * TIME_UNIT;
-constexpr const uint32_t PWM_RANGE = TIME_UNIT;
+constexpr const uint32_t TIME_UNIT = 1000;                  // 1000 us
+constexpr const uint32_t MEASURE_PERIOD = 50  * TIME_UNIT;  // 50 ms
+constexpr const uint32_t LOG_PERIOD = 500 * TIME_UNIT;      // 500 ms
+constexpr const uint32_t PWM_RANGE = 1 * TIME_UNIT;         // 1 ms
 
-uint64_t g_counter          = 0;
+uint64_t g_measure_time     = 0;
+uint64_t g_log_time         = 0;
+
 float    g_analog_averaged  = 0;
 
 uint32_t g_pwm_counter      = 0;
@@ -34,22 +35,17 @@ void setup() {
 }
 
 void loop() {
-    ++g_counter;
+    ulong time = micros();
 
-    float analogValue = 0;
-
-    if (g_counter % MEASURE_PERIOD == 0) {
-        analogValue = A_OFFSET + analogReadMilliVolts(ADC_PIN) / 1000.0f;
+    if (time > g_measure_time) {
+        float analogValue = analogReadMilliVolts(ADC_PIN) / 1000.0f;
         g_analog_averaged = (K - 1) * g_analog_averaged / K + analogValue / K;
-        g_pwm_duty = g_analog_averaged / A_REF * PWM_RANGE;
-        g_pwm_counter = PWM_RANGE;
+        g_pwm_duty = std::round(g_analog_averaged / A_REF * PWM_RANGE);
+        g_measure_time += MEASURE_PERIOD;
     }
 
-    if (g_pwm_counter++ == PWM_RANGE)
-        g_pwm_counter = 0;
-
     // Compute duty cycle
-    int pwm_step = g_pwm_counter % PWM_RANGE;
+    int pwm_step = g_pwm_counter++ % PWM_RANGE;
     if (pwm_step < g_pwm_duty) {
         // High level part
         if (!g_control) {
@@ -65,8 +61,9 @@ void loop() {
         }
     }
 
-    if (g_counter % LOG_PERIOD == 0) {
-        Serial.printf("mV: %0.3f, pwm_duty: %u\n", g_analog_averaged, g_pwm_duty);
+    if (time > g_log_time) {
+        Serial.printf("mV: %0.3f, PWM: %0.1f%%\n", g_analog_averaged, g_analog_averaged / A_REF * 100.0);
+        g_log_time += LOG_PERIOD;
     }    
 }
 
