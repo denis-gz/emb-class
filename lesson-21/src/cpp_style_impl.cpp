878890de573b36e6b09ebf32c28bf374006616c9
Denis Z <denis.gz@gmail.com>
2026-01-12 00:23:23 +0200

    [Lesson 21] Moved class impl to cpp file

--- lesson-21/src/cpp_style_impl.cpp
+++ lesson-21/src/cpp_style_impl.cpp
@@ -6,6 +6,34 @@ static LED_handler led_1_handler(6, 10, 200);
 static LED_handler led_2_handler(8, 10, 500);
 static LED_handler led_3_handler(12, 10, 1000);
 
+LED_handler::LED_handler(uint pin, uint on_duration, uint off_duration)
+	: pin(pin)
+	, on_duration(on_duration)
+	, off_duration(off_duration)
+{ }
+
+void LED_handler::schedule_work(ulong now_time) {
+	m_its_time = false;
+
+	if (now_time > m_next_on_time) {
+		m_next_off_time = m_next_on_time + on_duration;
+		m_logical_state = true;
+		m_its_time = true;
+	}
+	if (now_time > m_next_off_time) {
+		m_next_on_time = m_next_off_time + off_duration;
+		m_logical_state = false;
+		m_its_time = true;
+	}
+}
+
+void LED_handler::do_work() {
+	if (m_its_time) {
+		// The pin uses pull-up logic
+		digitalWrite(pin, m_logical_state ? LOW : HIGH);
+	}
+}
+
 void setup_cpp_impl()
 {
     pinMode(led_1_handler.pin, OUTPUT);
--- lesson-21/src/cpp_style_impl.h
+++ lesson-21/src/cpp_style_impl.h
@@ -5,33 +5,10 @@
 class LED_handler
 {
 public:
-	LED_handler(uint pin, uint on_duration, uint off_duration)
-		: pin(pin)
-		, on_duration(on_duration)
-		, off_duration(off_duration)
-	{ }
+	LED_handler(uint pin, uint on_duration, uint off_duration);
 
-	void schedule_work(ulong now_time) {
-		m_its_time = false;
-
-		if (now_time > m_next_on_time) {
-			m_next_off_time = m_next_on_time + on_duration;
-			m_logical_state = true;
-			m_its_time = true;
-		}
-		if (now_time > m_next_off_time) {
-			m_next_on_time = m_next_off_time + off_duration;
-			m_logical_state = false;
-			m_its_time = true;
-		}
-	}
-
-	void do_work() {
-		if (m_its_time) {
-			// The pin uses pull-up logic
-			digitalWrite(pin, m_logical_state ? LOW : HIGH);
-		}
-	}
+	void schedule_work(ulong now_time);
+	void do_work();
 
 public:	
 	const uint pin;
